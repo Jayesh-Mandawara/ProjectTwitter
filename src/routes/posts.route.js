19f0f1220bc184e4192338c47e355bc790b7391b
Jayesh Mandawara <jaymandawara08@gmail.com>
2026-04-02 00:22:50 +0530

    Added middlewares and updated the routes and the postController

--- src/routes/posts.route.js
+++ src/routes/posts.route.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const postRouter = express.Router();
-const createPostController = require("../controllers/posts.controller");
+const { createPostController, getPostController, getPostDetailsController } = require("../controllers/posts.controller");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
+const identifyUser = require("../middlewares/auth.middleware")
 
-postRouter.post("/", upload.single("image"), createPostController);
+
+postRouter.post("/", upload.single("image"), identifyUser, createPostController);
+
+postRouter.get("/", identifyUser, getPostController);
+
+postRouter.get('/details/:postId', identifyUser, getPostDetailsController);
 
 module.exports = postRouter;
