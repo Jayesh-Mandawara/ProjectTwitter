259293e5d2e7d332e37c095fa48427d57d25bee8
Jayesh Mandawara <150209576+Jayesh-Mandawara@users.noreply.github.com>
2026-04-04 00:21:23 +0530

    Clarify CORS configuration in app.js
    Added comments to clarify CORS configuration for credentials.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+// If we are passing credentials form the frontend then here also we have to accept them and provide the exact origin that from
+// where data is coming from
 app.use(
     cors({
         credentials: true,
